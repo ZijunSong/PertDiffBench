be2375b5b8cce3e5992ab283dcce03567cc5649a
Purple Nightmare <97300698+ZijunSong@users.noreply.github.com>
2025-08-22 20:23:09 +0800

    Delete scDiff directory

--- scDiff/scdiff/__init__.py
+++
@@ -1,3 +0,0 @@
-__all__ = ["config"]
-
-from scdiff import config
--- scDiff/scdiff/modules/diffusion_model/__init__.py
+++
@@ -1,9 +0,0 @@
-__all__ = [
-    "Decoder",
-    "Embedder",
-    "Encoder",
-]
-
-from scdiff.modules.diffusion_model.decoder import Decoder
-from scdiff.modules.diffusion_model.embedder import Embedder
-from scdiff.modules.diffusion_model.encoder import Encoder
